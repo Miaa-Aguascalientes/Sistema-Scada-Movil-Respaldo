623a09f2268e827959c2a402706973f887f7752c
Miaa <pedroguillermomiaa@gmail.com>
2026-05-27 14:50:37 -0600

    Update Miaa Scada Movil - Respaldo.py

--- Miaa Scada Movil - Respaldo.py
+++ Miaa Scada Movil - Respaldo.py
@@ -739,8 +739,7 @@ elif st.session_state.activo_tipo == "Tanque" and st.session_state.activo_id != 
         df['FECHA'] = pd.to_datetime(df['FECHA'])
         
         if not df.empty:
-            # 1. GRÁFICO HISTÓRICO
-            if not df.empty:
+
         # 1. GRÁFICO HISTÓRICO
         st.markdown("<h4 style='color:#00d4ff;'>📊 Nivel Histórico</h4>", unsafe_allow_html=True)
         fig1 = go.Figure(go.Scatter(x=df['FECHA'], y=df['VALUE'], line=dict(color='#00ffcc')))
