e61b878fe9aa9f9948d1cac92826c1f67824c559
Miaa <pedroguillermomiaa@gmail.com>
2026-06-01 09:33:25 -0600

    Update Miaa Scada Movil - Respaldo.py

--- Miaa Scada Movil - Respaldo.py
+++ Miaa Scada Movil - Respaldo.py
@@ -939,7 +939,7 @@ elif st.session_state.activo_tipo == "Rebombeo" and st.session_state.activo_id !
                     xanchor="left", # Anclaje al lado izquierdo
                     x=0             # Posición 0 (izquierda)
                 ),
-                margin=dict(t=60, b=80, l=10, r=10),
+                margin=dict(t=40, b=40, l=40, r=0),
             )
             
             fig_rb.update_traces(hovertemplate="%{y:.2f}")
